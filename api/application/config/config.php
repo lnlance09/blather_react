6e564d4e9374ca4993e8043d049393ba41428bd4
Lance Newman <lnewman@brightidea.com>
2018-09-29 21:58:20 -0400

    base url fix

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -24,7 +24,7 @@ define('ROOT_DIR', dirname(__FILE__));
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'ec2-18-236-66-36.us-west-2.compute.amazonaws.com';
+$config['base_url'] = 'http://ec2-18-236-66-36.us-west-2.compute.amazonaws.com';
 
 /*
 |--------------------------------------------------------------------------
