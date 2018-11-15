2e44ebe730d130697ff1898c593b032a49b0d784
Lance Newman <lnewman@brightidea.com>
2018-11-14 21:08:41 -0500

    Bug fix

--- api/public/js/blather/src/pages/index.js
+++ api/public/js/blather/src/pages/index.js
@@ -63,7 +63,7 @@ class Page extends Component {
 		const id = props.match.params.id
 		const network = props.match.params.network
 		const tab = props.match.params.tab
-		if (this.state.id !== id) {
+		if (this.props.id !== id) {
 			this.props.fetchPageData({
 				bearer: this.state.bearer,
 				id: id,
