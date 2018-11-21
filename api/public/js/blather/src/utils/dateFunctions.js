f0d1337a54ea5622bb58c88a3d70ab09f0518362
Lance Newman <lnewman@brightidea.com>
2018-11-20 21:48:00 -0500

    Safari date bug fix

--- api/public/js/blather/src/utils/dateFunctions.js
+++ api/public/js/blather/src/utils/dateFunctions.js
@@ -2,8 +2,13 @@ import React from "react"
 import Moment from "react-moment"
 
 const hoursOffset = new Date().getTimezoneOffset() / 60
-export const adjustTimezone = date => 
-	new Date(Date.parse(date) === undefined ? date.replace(/\s/, "T") : date).getTime() - hoursOffset * 3600000
+export const adjustTimezone = date => {
+	let dateStr = date
+	if(date !== undefined) {
+		dateStr = Date.parse(date) !== undefined ? date.replace(/-/g, "/") : date
+	}
+	return new Date(dateStr).getTime() - hoursOffset * 3600000
+}
 export const dateDifference = (dateOne, dateTwo) => (
 	<div>
 		<Moment from={adjustTimezone(dateOne)} ago>
