15e0800f9ddc024501f1be53897dc1789b063abe
Lance Newman <lnewman@brightidea.com>
2018-11-21 21:43:05 -0500

    Ran production build

--- index.php
+++ index.php
@@ -370,7 +370,7 @@
         </noscript>
         <div id="root"></div>
     </body>
-    <script src="/static/js/main.1be7059c.js"></script>
+    <script src="/static/js/main.aeb157cb.js"></script>
 <?php
     if($schema) {
 ?>
