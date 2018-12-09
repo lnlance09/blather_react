0f5ceb130cb7b92bf67772c29ea8696f0383096e
Lance Newman <lnewman@brightidea.com>
2018-12-09 13:55:46 -0500

    Ran production build

--- index.php
+++ index.php
@@ -340,7 +340,7 @@
 
         <meta name="description" content="<?php echo htmlentities($description); ?>" />
 
-        <link rel="stylesheet" type="text/css" href="/static/css/main.c5ddcf89.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.f469ee97.css">
         <link rel="manifest" href="manifest.json">
         <link rel="shortcut icon" href="/favicon.ico">
         <meta name="google-site-verification" content="bTDbvvxwQikYB9zsfufDiaqgVHMRi4DZ0311nJpngi8" />
@@ -370,7 +370,7 @@
         </noscript>
         <div id="root"></div>
     </body>
-    <script src="/static/js/main.c5c09141.js"></script>
+    <script src="/static/js/main.bbd41338.js"></script>
 <?php
     if($schema) {
 ?>
