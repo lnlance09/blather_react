543874739f1587eb0d95e4cab14c53d5c1ac20ee
Lance Newman <lnewman@brightidea.com>
2019-02-02 14:59:22 -0500

    Updated path to favicon

--- index.php
+++ index.php
@@ -348,7 +348,7 @@
 
         <link rel="stylesheet" type="text/css" href="static/css/main.21dde373.css">
         <link rel="manifest" href="manifest.json">
-        <link rel="shortcut icon" href="favicon.ico?v=1">
+        <link rel="shortcut icon" href="/favicon.ico?v=1">
         <meta name="google-site-verification" content="bTDbvvxwQikYB9zsfufDiaqgVHMRi4DZ0311nJpngi8" />
         <title><?php echo $title; ?> - Blather</title>
 
