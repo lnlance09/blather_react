076dc68ebb44f757c5cae60cbcdba7de63a67802
Lance Newman <lnewman@brightidea.com>
2019-02-04 20:10:10 -0500

    Ran production build

--- index.php
+++ index.php
@@ -346,7 +346,7 @@
 
         <meta name="description" content="<?php echo htmlentities($description); ?>" />
 
-        <link rel="stylesheet" type="text/css" href="/static/css/main.e8fc1c39.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.5433c565.chunk.css">
         <link rel="manifest" href="manifest.json">
         <link rel="shortcut icon" href="/favicon.ico?v=1">
 
@@ -378,7 +378,7 @@
         <div id="root"></div>
     </body>
     <script src="/static/js/1.aa086741.chunk.js"></script>
-    <script src="/static/js/main.17780865.chunk.js"></script>
+    <script src="/static/js/main.59318873.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 <?php
     if($schema) {
