7568de39fa0477600fb63e0f86c16d73579eab72
Lance Newman <lnewman@brightidea.com>
2019-02-23 22:53:51 -0500

    Ran production build

--- index.php
+++ index.php
@@ -379,7 +379,7 @@
         <div id="root"></div>
     </body>
     <script src="/static/js/1.bde46723.chunk.js"></script>
-    <script src="/static/js/main.34bf689a.chunk.js"></script>
+    <script src="/static/js/main.20a4d5a6.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 <?php
     if($schema) {
