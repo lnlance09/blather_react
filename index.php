342c668b23855733893fda7a0895d61c05895c9c
Lance Newman <lnewman@brightidea.com>
2019-02-24 19:54:56 -0500

    Ran production build

--- index.php
+++ index.php
@@ -346,7 +346,7 @@
         <meta property="og:url" content="https://blather.io<?php echo $uri; ?>" />
         <meta name="description" content="<?php echo htmlentities($description); ?>" />
 
-        <link rel="stylesheet" type="text/css" href="/static/css/main.9ad1ba86.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.d4bd768d.chunk.css">
         <link rel="manifest" href="/manifest.json">
         <link rel="shortcut icon" href="/favicon.ico?v=3">
         <link rel="apple-touch-icon" sizes="128x128" href="/favicon.ico?v=3">
@@ -377,8 +377,8 @@
         </noscript>
         <div id="root"></div>
     </body>
-    <script src="/static/js/1.bde46723.chunk.js"></script>
-    <script src="/static/js/main.3e1c360c.chunk.js"></script>
+    <script src="/static/js/1.33855ee9.chunk.js"></script>
+    <script src="/static/js/main.60f458c5.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 <?php
     if($schema) {
