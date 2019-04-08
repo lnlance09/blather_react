03479b27d5b80bd19d0d0dbada83cae08782c080
Lance Newman <lnewman@brightidea.com>
2019-04-07 21:06:26 -0400

    Ran production build
    Signed-off-by: Lance Newman <lnewman@brightidea.com>

--- index.php
+++ index.php
@@ -403,7 +403,7 @@
         <div id="root"></div>
     </body>
     <script src="/static/js/1.a0bce95b.chunk.js"></script>
-    <script src="/static/js/main.41a07e59.chunk.js"></script>
+    <script src="/static/js/main.7a885d7c.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 <?php
     if ($schema) {
