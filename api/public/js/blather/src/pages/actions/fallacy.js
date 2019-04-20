3d314ca98a6e885101ca6c3f005126ef2999736b
Lance Newman <lnewman@brightidea.com>
2019-04-19 22:48:19 -0400

    Bug fix with fallacy comments

--- api/public/js/blather/src/pages/actions/fallacy.js
+++ api/public/js/blather/src/pages/actions/fallacy.js
@@ -60,25 +60,6 @@ export const fetchCommentCount = ({ id }) => dispatch => {
 	)
 }
 
-export const fetchComments = ({ id, page }) => dispatch => {
-	request.get(
-		`${window.location.origin}/api/fallacies/getComments`,
-		{
-			json: true,
-			qs: {
-				id,
-				page
-			}
-		},
-		function(err, response, body) {
-			dispatch({
-				type: constants.GET_COMMENTS,
-				payload: body
-			})
-		}
-	)
-}
-
 export const fetchFallacyConversation = ({ bearer, id }) => dispatch => {
 	request.get(
 		`${window.location.origin}/api/fallacies/getConversation`,
@@ -115,28 +96,6 @@ export const fetchFallacy = ({ bearer, id }) => dispatch => {
 		function(err, response, body) {
 			dispatch({
 				type: constants.GET_FALLACY,
-				payload: body
-			})
-		}
-	)
-}
-
-export const postComment = ({ bearer, id, message }) => dispatch => {
-	request.post(
-		`${window.location.origin}/api/fallacies/postComment`,
-		{
-			form: {
-				id,
-				message
-			},
-			headers: {
-				Authorization: bearer
-			},
-			json: true
-		},
-		function(err, response, body) {
-			dispatch({
-				type: constants.POST_COMMENT,
 				payload: body
 			})
 		}
