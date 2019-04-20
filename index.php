f20efa0d2041b4926f5afd48e04b08c01ae4dc8d
Lance Newman <lnewman@brightidea.com>
2019-04-20 18:32:23 -0400

    Ran production build

--- index.php
+++ index.php
@@ -371,7 +371,7 @@
     }
 ?>
 
-        <link rel="stylesheet" type="text/css" href="/static/css/main.32db034f.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.6ca56a87.chunk.css">
         <link rel="manifest" href="/manifest.json">
         <link rel="shortcut icon" href="/favicon.ico?v=3">
         <link rel="apple-touch-icon" sizes="128x128" href="/favicon.ico?v=3">
@@ -402,8 +402,8 @@
         </noscript>
         <div id="root"></div>
     </body>
-    <script src="/static/js/1.88b92901.chunk.js"></script>
-    <script src="/static/js/main.51381aff.chunk.js"></script>
+    <script src="/static/js/1.1d7e04ca.chunk.js"></script>
+    <script src="/static/js/main.3223db1d.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 <?php
     if ($schema) {
