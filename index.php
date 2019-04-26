b99e548cd61c5c809f900f44ce8772ec216fd654
Lance Newman <lnewman@brightidea.com>
2019-04-26 16:03:04 -0400

    Removed uneccessary JS file from index page

--- index.php
+++ index.php
@@ -598,7 +598,6 @@
     <script src="/static/js/1.63a53413.chunk.js"></script>
     <script src="/static/js/main.00ae4be8.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
-    <script src="static/js/main.c3e74a8e.js"></script>
 
 <?php
     if ($schema) {
