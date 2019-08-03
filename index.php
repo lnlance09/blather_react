69536ea3919252a922a227bb0ea0c12fae0fd116
Lance Newman <lnewman@brightidea.com>
2019-08-02 22:53:42 -0400

    Prod build

--- index.php
+++ index.php
@@ -611,7 +611,7 @@
     </body>
 
     <script src="/static/js/1.cd773763.chunk.js"></script>
-    <script src="/static/js/main.116edb10.chunk.js"></script>
+    <script src="/static/js/main.ea9ed4bd.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
 <?php
