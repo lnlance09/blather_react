a751ec297418b588e0cb55e4b99c1329db86a0e9
Lance Newman <lnewman@brightidea.com>
2019-08-05 14:28:57 -0400

    Prod build

--- index.php
+++ index.php
@@ -637,7 +637,7 @@
     }
 ?>
 
-        <link rel="stylesheet" type="text/css" href="/static/css/main.8431748a.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.e733711a.chunk.css">
         <link rel="manifest" href="/manifest.json">
         <link rel="shortcut icon" href="/favicon.ico?v=3">
         <link rel="apple-touch-icon" sizes="128x128" href="/favicon.ico?v=3">
@@ -656,7 +656,7 @@
     </body>
 
     <script src="/static/js/1.b382ada4.chunk.js"></script>
-    <script src="/static/js/main.c9bfdce7.chunk.js"></script>
+    <script src="/static/js/main.c903c983.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
 <?php
