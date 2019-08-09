3d7ce24eb79297e8af382edf3a8693269718eac2
Lance Newman <lnewman@brightidea.com>
2019-08-09 12:13:42 -0400

    Fixed bug

--- api/application/controllers/Twitter.php
+++ api/application/controllers/Twitter.php
@@ -234,7 +234,7 @@
 				'data' => $tweet['error'] ? null : $tweet['data'],
 				'error' => $tweet['error'] ? true : false,
 				'is_live_search' => $auth,
-				'profile_pic' => !$tweet['error'] && !$auth ? $tweet['profile_pic'] : null,
+				'profile_pic' => !$tweet['error'] ? $tweet['profile_pic'] : null,
 				'type' => 'tweet'
 			]);
 		}
