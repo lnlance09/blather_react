b15801ac5d9c4d777dc15d46519a991ba28f2dd2
Lance Newman <lnewman@brightidea.com>
2019-09-03 21:41:24 -0400

    Updated sitemap

--- api/application/views/sitemap.php
+++ api/application/views/sitemap.php
@@ -18,6 +18,38 @@
 
 
 <?php
+	// Twitter pages
+	for ($i=0;$i<count($twitterPages);$i++) {
+		$username = $twitterPages[$i]['username'];
+		$url = $base_url.'pages/twitter/'.$username;
+?>
+	<url>
+		<loc><?php echo $url; ?></loc>
+		<changefreq>daily</changefreq>
+		<priority>0.9</priority>
+	</url>
+<?php
+	}
+?>
+
+
+<?php
+	// Youtube pages
+	for ($i=0;$i<count($youtubePages);$i++) {
+		$id = $youtubePages[$i]['social_media_id'];
+		$url = $base_url.'pages/youtube/'.$id;
+?>
+	<url>
+		<loc><?php echo $url; ?></loc>
+		<changefreq>daily</changefreq>
+		<priority>0.9</priority>
+	</url>
+<?php
+	}
+?>
+
+
+<?php
 	// Fallacies
 	for ($i=0;$i<count($fallacies);$i++) {
 		$slug = $fallacies[$i]['slug'];
@@ -26,7 +58,7 @@
 	<url>
 		<loc><?php echo $url; ?></loc>
 		<changefreq>daily</changefreq>
-		<priority>0.9</priority>
+		<priority>0.8</priority>
 	</url>
 <?php
 	}
@@ -76,38 +108,6 @@
 
 
 <?php
-	// Twitter pages
-	for ($i=0;$i<count($twitterPages);$i++) {
-		$username = $twitterPages[$i]['username'];
-		$url = $base_url.'pages/twitter/'.$username;
-?>
-	<url>
-		<loc><?php echo $url; ?></loc>
-		<changefreq>daily</changefreq>
-		<priority>0.6</priority>
-	</url>
-<?php
-	}
-?>
-
-
-<?php
-	// Youtube pages
-	for ($i=0;$i<count($youtubePages);$i++) {
-		$id = $youtubePages[$i]['social_media_id'];
-		$url = $base_url.'pages/youtube/'.$id;
-?>
-	<url>
-		<loc><?php echo $url; ?></loc>
-		<changefreq>daily</changefreq>
-		<priority>0.6</priority>
-	</url>
-<?php
-	}
-?>
-
-
-<?php
 	// Fallacy types
 	for ($i=0;$i<count($fallacyTypes);$i++) {
 		$replace = str_replace(' ', '-', $fallacyTypes[$i]['name']);
@@ -116,7 +116,7 @@
 	<url>
 		<loc><?php echo $base_url.'fallacies/'.$name; ?></loc>
 		<changefreq>daily</changefreq>
-		<priority>0.4</priority>
+		<priority>0.5</priority>
 	</url>
 <?php
 	}
@@ -135,6 +135,11 @@
 	</url>
 	<url>
 		<loc><?php echo $base_url; ?>privacy</loc>
+		<changefreq>never</changefreq>
+		<priority>0.3</priority>
+	</url>
+	<url>
+		<loc><?php echo $base_url; ?>rules</loc>
 		<changefreq>never</changefreq>
 		<priority>0.3</priority>
 	</url>
