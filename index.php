5b2b2798a32825267c0524e65bb3fa99bada0b31
Lance Newman <lnewman@brightidea.com>
2019-09-21 22:03:47 -0400

    Prod build

--- index.php
+++ index.php
@@ -733,7 +733,7 @@
     </body>
 
     <script src="/static/js/1.619a0da6.chunk.js"></script>
-    <script src="/static/js/main.14fa5bbc.chunk.js"></script>
+    <script src="/static/js/main.c15dcf27.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
     <script>
