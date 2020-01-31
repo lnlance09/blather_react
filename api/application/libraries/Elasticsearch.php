74338a29b858dad47d38d5f31364b04d41a4cef2
Lance Newman <lnewman@brightidea.com>
2020-01-30 19:59:08 -0500

    Added ES page

---
+++ api/application/libraries/Elasticsearch.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH.'vendor/autoload.php';
+use Elasticsearch\ClientBuilder;
+
+class Elasticsearch {
+	public function indexDoc($index, $id, $body) {
+		$client = ClientBuilder::create()->build();
+		$params = [
+			'index' => $index,
+			'id' => $id,
+			'body' => $body
+		];
+
+		$response = $client->index($params);
+		return $response;
+	}
+
+	public function getDoc($index, $id) {
+		$client = ClientBuilder::create()->build();
+		$params = [
+			'index' => $index,
+			'id' => $id
+		];
+
+		$response = $client->get($params);
+		return $response;
+	}
+
+	public function searchDocs($index, $body) {
+		$client = ClientBuilder::create()->build();
+		$params = [
+			'index' => $index,
+			'body' => $body
+		];
+
+		$response = $client->search($params);
+		return $response;
+	}
+}
