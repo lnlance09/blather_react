3a5d5584d90cfdb7eb321fb91522c5972c82f516
Lance Newman <lnewman@brightidea.com>
2020-04-24 18:29:49 -0400

    Prod build

--- index.php
+++ index.php
@@ -701,7 +701,7 @@
 ?>
 
         <link rel="stylesheet" type="text/css" href="/static/css/1.fd40afcb.chunk.css">
-        <link rel="stylesheet" type="text/css" href="/static/css/main.7f946372.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.e40aeca6.chunk.css">
 
         <link rel="canonical" href="<?php echo $canonical_url; ?>" />
         <link rel="home" href="<?php echo $base_url; ?>" />
@@ -734,7 +734,7 @@
     </body>
 
     <script src="/static/js/1.dce608df.chunk.js"></script>
-    <script src="/static/js/main.7ecf9065.chunk.js"></script>
+    <script src="/static/js/main.60051444.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
     <script>
