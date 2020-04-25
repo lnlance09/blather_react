3456a5f52bd7246cec2ce8e678a8f660f0b1f9e2
Lance Newman <lnewman@brightidea.com>
2020-04-24 22:14:03 -0400

    Changed AWS access keys

--- api/application/config/s3config.php
+++ api/application/config/s3config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['s3key'] = 'AKIA3KB7ZZF2YCN4MDJ4';
-$config['s3secret'] = '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';
+$config['s3key'] = 'AKIA3KB7ZZF26C4NLY6O';
+$config['s3secret'] = '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';
 $config['s3bucket'] = 'blather22';
