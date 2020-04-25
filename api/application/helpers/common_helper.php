2e3374f51cb95a1e4dd8e00293dc20a97deb187f
Lance Newman <lnewman@brightidea.com>
2020-04-25 00:22:25 -0400

    Curl fix

--- api/application/helpers/common_helper.php
+++ api/application/helpers/common_helper.php
@@ -58,6 +58,8 @@
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0');
+		curl_setopt($curl, CURLOPT_REFERER, 'https://youtube.com/');
 		$data = curl_exec($ch);
 		curl_close($ch);
 		return $data;
