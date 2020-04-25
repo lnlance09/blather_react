f76136445f5022f48b2d38a5fbed27c71dcd0764
Lance Newman <lnewman@brightidea.com>
2020-04-25 00:11:16 -0400

    Changed the way subtitles are retrieved

--- api/application/helpers/common_helper.php
+++ api/application/helpers/common_helper.php
@@ -53,6 +53,16 @@
 		return $code;
 	}
 
+	function curlGetContents($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+
 	/**
 	 * Format the default text for the 'about' section of a page
 	 * @param  [string] $about [description]
