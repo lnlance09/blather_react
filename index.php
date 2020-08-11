ee448912f47f44bea3a81e013d5d874750a55c11
Lance Newman <lnewman@brightidea.com>
2020-08-11 09:46:41 -0400

    Prod build

--- index.php
+++ index.php
@@ -711,7 +711,7 @@
 ?>
 
         <link rel="stylesheet" type="text/css" href="/static/css/1.afd5d2df.chunk.css">
-        <link rel="stylesheet" type="text/css" href="/static/css/main.86bed8e8.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.372f8c7d.chunk.css">
 
         <link rel="canonical" href="<?php echo $canonical_url; ?>" />
         <link rel="home" href="<?php echo $base_url; ?>" />
@@ -743,8 +743,8 @@
         </div>
     </body>
 
-    <script src="/static/js/1.60e85972.chunk.js"></script>
-    <script src="/static/js/main.ca13a219.chunk.js"></script>
+    <script src="/static/js/1.a51ffc77.chunk.js"></script>
+    <script src="/static/js/main.1e29996d.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
     <script>
