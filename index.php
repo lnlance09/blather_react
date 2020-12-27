b16108106d958febf81c5d893e85be983674d7df
Lance Newman <lnewman@brightidea.com>
2020-12-26 21:15:27 -0500

    Ran prod build

--- index.php
+++ index.php
@@ -732,8 +732,8 @@
     }
 ?>
 
-        <link rel="stylesheet" type="text/css" href="/static/css/1.12cc540d.chunk.css">
-        <link rel="stylesheet" type="text/css" href="/static/css/main.8418a29f.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/1.7d258906.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.6faacd60.chunk.css">
 
         <link rel="canonical" href="<?php echo $canonical_url; ?>" />
         <link rel="home" href="<?php echo $base_url; ?>" />
@@ -765,8 +765,8 @@
         </div>
     </body>
 
-    <script src="/static/js/1.ddc3acd2.chunk.js"></script>
-    <script src="/static/js/main.720fffcb.chunk.js"></script>
+    <script src="/static/js/1.a7fe6011.chunk.js"></script>
+    <script src="/static/js/main.2f182cee.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
     <script>
