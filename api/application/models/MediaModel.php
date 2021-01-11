1f552b36d37ca31147f8752c4881929edc6e6021
Lance Newman <lnewman@brightidea.com>
2021-01-11 14:21:48 -0500

    bug fix

--- api/application/models/MediaModel.php
+++ api/application/models/MediaModel.php
@@ -195,6 +195,8 @@ class MediaModel extends CI_Model {
 			$command .= ' https://www.youtube.com/watch\?v\='.$video_id;
 			$command .= ' --verbose 2>&1';
 			exec($command, $output);
+			echo $command;
+			var_dump($output);
 		}
 
 		return $file;
