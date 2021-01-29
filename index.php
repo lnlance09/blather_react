d545bcd4126210a82922d32cbaeacc9b78020d3f
Lance Newman <lnewman@brightidea.com>
2021-01-29 16:51:25 -0500

    bug fix

--- index.php
+++ index.php
@@ -770,7 +770,7 @@
     </body>
 
     <script src="/static/js/1.49961f8b.chunk.js"></script>
-    <script src="/static/js/main.729244df.chunk.js"></script>
+    <script src="/static/js/main.1d7f0af1.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
     <script>
