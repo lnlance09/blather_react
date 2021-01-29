a802105bdbf2f067cfb2ba4abdb209f9058024d2
Lance Newman <lnewman@brightidea.com>
2021-01-29 15:00:23 -0500

    Prod build

--- index.php
+++ index.php
@@ -770,7 +770,7 @@
     </body>
 
     <script src="/static/js/1.5b589b7e.chunk.js"></script>
-    <script src="/static/js/main.966d14e4.chunk.js"></script>
+    <script src="/static/js/main.ee378647.chunk.js"></script>
     <script src="/static/js/runtime~main.229c360f.js"></script>
 
     <script>
